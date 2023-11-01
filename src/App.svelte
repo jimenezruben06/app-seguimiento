a67049b25c02e84f0d733929927a3fd5947976ea
José Miguel Pineda Cedano <josemiguelpinedac@gmail.com>
2023-11-01 12:05:00 -0400

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Navbar from "./lib/Navbar.svelte";
+</script>
+
+<header>
+  <div>
+    <h1 class="app-name">App de Seguimiento</h1>
+  </div>
+</header>
+
+<nav>
+  <div>
+    <Navbar />
+  </div>
+</nav>
+
+<main>
+  <div>
+  </div>
+</main>
+
+<style>
+  .app-name {
+    float: left;
+    margin-left: 50px;
+  }
+
+  nav {
+    margin-top: 25px;
+    margin-left: 50px;
+  }
+</style>
---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+
+<div id="nav-list">
+    <li class="nav-items">Tasks</li>
+    <li class="nav-items">Reminders</li>
+</div>
+
+<style>
+    #nav-list {
+        float: right;
+        margin-right: 50px;
+    }
+
+    .nav-items{
+        text-decoration: none;
+        display: inline;
+        margin: 50px;
+        font-size: 25px;
+    }
+</style>
